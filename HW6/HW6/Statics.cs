f0af47321d842ba1dd6110e56f6bf3ca50b43b50
cschackel <cschackel@my.wctc.edu>
2020-02-29 16:04:49 -0600

    Add project files.

---
+++ HW6/HW6/Statics.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace HW6
+{
+    public static class Statics
+    {
+        public static readonly Color StaticColor=Color.Aqua;
+        public static readonly String WelcomeMessage = "Spam Alerts!";
+    }
+}
